9b0c33c880cb0e179cd22343bf966d49bc759d37
Tifani Warnita <tiffayumuyuka@gmail.com>
2017-03-26 08:11:41 -0700

    Add setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+# sudo -H pip3 install nltk
+# sudo -H pip3 install PyEnchant
+# python3 -m nltk.downloader punkt
+# python3 -m nltk.downloader stopwords
+# python3 -m nltk.downloader wordnet
+sudo -H pip3 install python-weka-wrapper3
