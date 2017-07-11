64496d604e49087616cc8b125fd9a0de7d685b1d
Tifani Warnita <tiffayumuyuka@gmail.com>
2017-07-10 23:08:33 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
+pip3 install virtualenv
+virtualenv hoaxanalyzer
+source hoaxanalyzer/bin/activate
 sudo apt-get install python3-numpy python3-nltk
 sudo apt-get install python3-pip python-dev build-essential
-sudo -H pip3 install PyEnchant
+sudo apt-get install libenchant1c2a
+pip3 install PyEnchant
+pip3 install -U nltk
 python3 -m nltk.downloader punkt
 python3 -m nltk.downloader stopwords
 python3 -m nltk.downloader wordnet
 python3 -m nltk.downloader averaged_perceptron_tagger
-sudo -H pip3 install python-weka-wrapper3
-sudo -H pip3 install scikit-learn
-sudo -H pip3 install numpy
-sudo -H pip3 install pandas
-sudo -H pip3 install validators
-sudo -H pip3 install newspaper3k
+pip3 install scikit-learn
+pip3 install numpy
+pip3 install pandas
+pip3 install validators
+pip3 install newspaper3k
+sudo apt-get install openmpi-bin
+pip3 install https://cntk.ai/PythonWheel/CPU-Only/cntk-2.0-cp35-cp35m-linux_x86_64.whl
+pip3 install regex
+pip3 install langdetect
+pip3 install -U pattern
+pip3 install Flask
+pip3 install flask-restful
